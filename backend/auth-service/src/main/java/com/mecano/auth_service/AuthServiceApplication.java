6109427ab4ce5450e7ced3480af4a052093b8e0b
djikeflorinda6 <djikeflorinda6@gmail.com>
2026-05-29 05:04:48 +0100

    feat: connexion auth-service à Eureka

--- backend/auth-service/src/main/java/com/mecano/auth_service/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/mecano/auth_service/AuthServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
